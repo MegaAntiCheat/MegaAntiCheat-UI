5bf378b755d2c27ee0db48f18b2e35df593b1c97
SammCheese <sammy@sammcheese.net>
2023-07-20 08:44:32 +0200

    scoreboard: add team player counter, seperate nav from players

--- src/components/TF2/ScoreboardTable/ScoreboardTable.tsx
+++ src/components/TF2/ScoreboardTable/ScoreboardTable.tsx
@@ -13,14 +13,14 @@ const ScoreboardTable = ({ BLU, RED }: ScoreboardTableType) => {
   return (
     <div className="scoreboard-grid-container">
       <div>
-        <div className="scoreboard-header blu">BLU</div>
+        <div className="scoreboard-header blu">BLU ({BLU?.length ?? 0})</div>
+        <Flex className="scoreboard-nav">
+          <div>{t('TEAM_NAV_RATING')}</div>
+          <div>{t('TEAM_NAV_USER')}</div>
+          <div>{t('TEAM_NAV_STATUS')}</div>
+          <div>{t('TEAM_NAV_TIME')}</div>
+        </Flex>
         <div className="scoreboard-team">
-          <Flex className="scoreboard-nav">
-            <div>{t('TEAM_NAV_RATING')}</div>
-            <div>{t('TEAM_NAV_USER')}</div>
-            <div>{t('TEAM_NAV_STATUS')}</div>
-            <div>{t('TEAM_NAV_TIME')}</div>
-          </Flex>
           {BLU?.map((player) => {
             return (
               <Player className="blu" player={player} key={player.steamID64} />
@@ -30,14 +30,14 @@ const ScoreboardTable = ({ BLU, RED }: ScoreboardTableType) => {
       </div>
       <div className="scoreboard-divider" />
       <div>
-        <div className="scoreboard-header red">RED</div>
+        <div className="scoreboard-header red">RED ({RED?.length ?? 0})</div>
+        <Flex className="scoreboard-nav">
+          <div>{t('TEAM_NAV_RATING')}</div>
+          <div>{t('TEAM_NAV_USER')}</div>
+          <div>{t('TEAM_NAV_STATUS')}</div>
+          <div>{t('TEAM_NAV_TIME')}</div>
+        </Flex>
         <div className="scoreboard-team">
-          <Flex className="scoreboard-nav">
-            <div>{t('TEAM_NAV_RATING')}</div>
-            <div>{t('TEAM_NAV_USER')}</div>
-            <div>{t('TEAM_NAV_STATUS')}</div>
-            <div>{t('TEAM_NAV_TIME')}</div>
-          </Flex>
           {RED?.map((player) => {
             return (
               <Player className="red" player={player} key={player.steamID64} />
