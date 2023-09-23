aebac328ac627c458a465dfe8af553c22a278111
SammCheese <sammy@sammcheese.net>
2023-09-23 21:53:59 +0200

    better dev mode separation

--- src/api/globals/index.ts
+++ src/api/globals/index.ts
@@ -11,7 +11,7 @@ export const COMMAND_ENDPOINT = `${APIURL}/commands/v1`;
 
 // This is used to get fakedata for the frontend.
 // dotenv breaks build, so we can't use that.
-export const useFakedata = false;
+export const useFakedata = process.env.NODE_ENV?.includes('development');
 
 export async function verifyBackend(): Promise<boolean> {
   return await fetch(SERVERFETCH)
