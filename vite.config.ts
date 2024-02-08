00070e0dfe2134c084b8d1e843483d7dd7eacfb6
meat <meatloaf.tf@gmail.com>
2024-02-08 14:53:07 +1000

    Deleted unused index.html, fixed vite.config not serving public assets properly

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   root: './src',
+  base: '/',
+  build: {
+    outDir: '../dist',
+  },
+  publicDir: '../public',
   resolve: {
     alias: {
       '@api': path.resolve(__dirname, './src/api'), // replace with actual path
