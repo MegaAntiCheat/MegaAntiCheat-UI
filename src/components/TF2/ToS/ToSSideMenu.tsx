43d12947813bc35000600f7fc80d32eed5c07b39
Luke <luke.stephens@trintel.co.za>
2024-08-12 18:25:54 +0200

    add TOS side menu item

--- src/components/TF2/ToS/ToSSideMenu.tsx
+++ src/components/TF2/ToS/ToSSideMenu.tsx
@@ -2,20 +2,25 @@ import { t } from '@i18n';
 import { AlertTriangle } from 'lucide-react';
 import { SideMenuItem } from '@components/General';
 import { PAGES } from '../../../constants/menuConstants';
+import { useSideMenu } from '../../../Context/SideMenuContext';
+import { Dispatch, SetStateAction } from 'react';
 
 interface ProvisionProps {
   collapsed: boolean;
-  setCurrentPage?: React.Dispatch<React.SetStateAction<PAGES>>;
+  setCurrentPage?: Dispatch<SetStateAction<PAGES>>;
 }
 
-function ToSSideMenu({ collapsed, setCurrentPage }: ProvisionProps) {
+function ToSSideMenu({ setCurrentPage }: ProvisionProps) {
+  const { toggleCollapsed } = useSideMenu();
+
   return (
     <SideMenuItem
-      key={68}
       title={t('TOS_HINT')}
-      Icon={<AlertTriangle color="yellow" className="bounce" />}
-      collapsed={collapsed}
-      onClick={() => setCurrentPage?.(PAGES.PREFERENCES)}
+      icon={<AlertTriangle color="yellow" className="bounce" />}
+      onClick={() => {
+        setCurrentPage?.(PAGES.PREFERENCES);
+        toggleCollapsed();
+      }}
       selected={false}
     />
   );
