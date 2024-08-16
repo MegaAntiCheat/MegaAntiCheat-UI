ee89aeebbecdf697a18ffcf1d147ff4d2223b5b5
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2024-08-16 16:32:12 -0300

    Initial translation phase

--- i18n/index.ts
+++ i18n/index.ts
@@ -9,6 +9,7 @@ import ja_RYU from './ja_RYU.json';
 import uk_UA from './uk_UA.json';
 import nl_NL from './nl_NL.json';
 import ko_KR from './ko_KR.json';
+import pt_BR from './pt_BR.json';
 
 import { tos_EN } from './tos/en_US.js';
 
@@ -29,6 +30,7 @@ const translations: { [key: string]: Translations } = {
   Dutch: nl_NL,
   Korean: ko_KR,
   French: fr_FR,
+  Portuguese: pt_BR
 };
 
 const setLanguage = (lang: string) => {
